435ac1817d01d76a0bada4ae13e7ba7322c97d6d
Ayu <claire@ayu.land>
2023-11-25 21:59:12 +0800

    Interrupt by DRDY

--- src/main.c
+++ src/main.c
@@ -81,6 +81,8 @@ int main()
 
   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);
+
   // ======== SPIx ADC ========
   // ADC_CK
   gpio_init.Pin = GPIO_PIN_11;
@@ -90,19 +92,23 @@ int main()
   HAL_GPIO_Init(GPIOA, &gpio_init);
   // ADC_DA
   gpio_init.Pin = GPIO_PIN_12;
-  gpio_init.Mode = GPIO_MODE_INPUT;
+  gpio_init.Mode = GPIO_MODE_IT_FALLING;
   gpio_init.Pull = GPIO_NOPULL;
   gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOA, &gpio_init);
 
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);
+  HAL_NVIC_SetPriority(EXTI4_15_IRQn, 2, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
+
   HAL_Delay(100);
   // Pull down clock / chip select signal and wait for DRDY
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET);
+/*
   uint32_t start = HAL_GetTick();
   while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_12) == GPIO_PIN_SET) { }
   swv_printf("Set-up = %u ms\n", HAL_GetTick() - start);  // 280 ms
   swv_printf("Clock = %u Hz\n", HAL_RCC_GetHCLKFreq());   // 16 MHz
+*/
 
   while (true) {
     HAL_GPIO_WritePin(LED_PORT, LED_PIN_ACT, GPIO_PIN_RESET);
@@ -116,4 +122,20 @@ void SysTick_Handler()
 {
   HAL_IncTick();
   HAL_SYSTICK_IRQHandler();
+}
+
+void EXTI4_15_IRQHandler()
+{
+  __HAL_GPIO_EXTI_CLEAR_FALLING_IT(GPIO_PIN_12);
+
+  static uint32_t ticks[20];
+  static int ticks_count = 0;
+  if (ticks_count < 20) {
+    ticks[ticks_count++] = HAL_GetTick();
+    if (ticks_count == 20)
+      // 100 ms (initial delay in program) + 300 (initial set-up) + 100 * i (10 Hz)
+      for (int i = 0; i < 20; i++) {
+        swv_printf("tick = %u\n", ticks[i]);
+      }
+  }
 }
